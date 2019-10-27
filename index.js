cc120b2148ac3e0a158bdefe7d2b829827b064d4
Dan Hintz <dhintz@live.com>
2019-10-27 20:07:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+
+// declaration  (hoisted)
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+// expression  (not hoisted)
+let fn = function(activity) {
+    return `This Monday, I will ${activity}.`
+}
+
+// Anonymous
+function() {
+    return "This type of function doesn't have a name"
+}
+
+// IIFE (Instantly-Invoked Function Expression)  (hides scope within)
+(function(x){return 2+x})(4)   // returns 6 
+
+// scope chain
+function wrapAdjective(flair = "*") {
+    return function(adj = "special") {
+        return `You are ${flair}${adj}${flair}!`
+    }
+}
+wrapAdjective("?")("confusing") // "You are ?confusing?!"
